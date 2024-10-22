b8c0d38dadf933dba7356ab59571d9c3f8eed0a8
Andrea Serrano <133468366+andrea-s7@users.noreply.github.com>
2024-10-22 11:42:33 -0700

    Add files via upload

---
+++ MainView.swift
@@ -0,0 +1,26 @@
+//
+//  MainView.swift
+//  3390-Project2
+//
+//  Created by Andrea Serrano  on 10/22/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct MainView: View {
+    //@Binding var isUserLoggedIn: Bool
+
+    var body: some View {
+        VStack {
+            Text("Main View")
+                .font(.largeTitle)
+                .padding()
+        }
+    }
+}
+
+#Preview{
+    MainView()
+}
+
